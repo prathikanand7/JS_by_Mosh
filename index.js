6a947e40486a1d0e734768d42d2a93f99a823059
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2024-02-13 12:10:23 +0530

    added text file

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 // Represents a comment - why and how this code is written - should be the aim of the comment
 console.log("hello world");
         
-//use let for all variable - they cannot be a reserved keyword. 
-/* They should be meaningful.
-Cannot start with a number.
-Cannot contain any space or hypen(-)
-They are case-sensitive
-use camel case - let firstName
- */
+
 
 let firstName = 'Ajay';
 let lastName = "Anand";
@@ -16,6 +10,12 @@ console.log(firstName);
 const interestRate = 0.3;
 console.log(interestRate);
 
-//Variale types: primitives / Value types - string, bool, int, undefined, null .
-//Variable types: reference types
+
+
+let age = 30;
+let isApproved = true;
+let firstNameBot = null;
+let secondNameBot = undefined;
+
+
 
